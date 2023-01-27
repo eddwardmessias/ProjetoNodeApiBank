3e769db97fb3706c3c5a96d085b300e384983ba9
eddwardmessias <eddwardmessias@hotmail.com>
2023-01-27 00:21:58 -0300

    Enviando alterações

--- src/index.js
+++ src/index.js
@@ -1,24 +1,31 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json())
 
-app.get("/courses", (req,res)=>{
+app.get("/courses", (req, res) => {
+    const query = req.query;
+    console.log(query);
     return res.json(["Java", "JavaScript", "Angular"])
 });
 
-app.post("/courses",(req,res)=>{
+app.post("/courses", (req, res) => {
+    const body = req.body;
+    console.log(body);
     return res.json(["Java", "JavaScript", "Angular", "React"])
 })
 
-app.put("/courses/:id", (req,res)=>{
+app.put("/courses/:id", (req, res) => {
+    const {id} = req.params;
+    console.log({id});
     return res.json(["NodeJS", "JavaScript", "Angular", "React"])
 })
 
-app.patch("/courses/:id", (req,res)=>{
+app.patch("/courses/:id", (req, res) => {
     return res.json(["NodeJS", "JavaScript", "Angular", "ReactNative"])
 })
 
-app.delete("/courses/:id", (req,res)=>{
+app.delete("/courses/:id", (req, res) => {
     return res.json(["Angular"])
 })
 
